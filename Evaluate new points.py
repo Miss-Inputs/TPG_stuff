89c4dc250e219e92b674509bab0065df36a1eba7
Megan Leet <megan.leet@outlook.com.au>
2025-09-29 10:00:07 +1000

    Lots of improvements to evaluate new points that I forgot to commit so I don't remember exactly what I did whoops

--- Evaluate new points.py
+++ Evaluate new points.py
@@ -2,15 +2,21 @@
 """Given an existing set of points (photos that one has for submitting to TPG, etc), and a set of new points to be added (e.g. new destinations to consider travelling to), finds out what is the most optimal."""
 
 import logging
-from argparse import ZERO_OR_MORE, ArgumentParser
+from argparse import ZERO_OR_MORE, ArgumentParser, BooleanOptionalAction
 from pathlib import Path
 
 import geopandas
+import pandas
 from pandas import Index, RangeIndex
 from shapely import Point
 from tqdm.auto import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
-from travelpygame.new_pic_eval import find_if_new_pics_better, load_points_or_rounds
+from travelpygame.new_pic_eval import (
+	find_if_new_pics_better,
+	find_new_pics_better_individually,
+	get_worst_point,
+	load_points_or_rounds,
+)
 from travelpygame.util import (
 	format_distance,
 	format_point,
@@ -46,6 +52,69 @@ def get_distances(points: geopandas.GeoDataFrame, new_points: geopandas.GeoDataF
 	)
 
 
+def get_where_pics_better(
+	points: geopandas.GeoDataFrame,
+	new_points: geopandas.GeoDataFrame,
+	targets: geopandas.GeoDataFrame,
+	*,
+	use_haversine: bool = True,
+):
+	results = find_if_new_pics_better(points, new_points, targets, use_haversine=use_haversine)
+	better = results[results['is_new_better']].copy()
+	better['diff'] = better['current_distance'] - better['new_distance']
+	return better.sort_values('diff', ascending=False)
+
+
+def eval_with_targets(
+	points: geopandas.GeoDataFrame,
+	new_points: geopandas.GeoDataFrame,
+	target_paths: list[Path],
+	*,
+	use_haversine: bool = True,
+	find_if_any_pics_better: bool = True,
+):
+	"""The function name kinda sucks but if it starts with test_ then Ruff thinks it's a test function and complains about things accordingly"""
+	targets = load_points_or_rounds(target_paths)
+	targets = try_set_index_name_col(targets)
+	if isinstance(targets.index, RangeIndex):
+		# Try and get something more descriptive than just the default increasing index
+		targets.index = Index(
+			[format_point(geo) if isinstance(geo, Point) else str(geo) for geo in targets.geometry]
+		)
+
+	if find_if_any_pics_better:
+		better = get_where_pics_better(points, new_points, targets, use_haversine=use_haversine)
+		# TODO: This should be saved to csv, it's just like, do I have a new output_path argument or what
+		for index, row in better.iterrows():
+			diff = row['diff']
+			new_pic = row['new_best']
+			old_pic = row['current_best']
+			print(
+				f'{index} would be improved by {new_pic}, beating {old_pic} by {format_distance(diff)}'
+			)
+
+	worst_target, worst_dist, pic_for_worst = get_worst_point(
+		points, targets, use_haversine=use_haversine
+	)
+	print(f'Worst case target: {worst_target}, {format_distance(worst_dist)} from {pic_for_worst}')
+	combined = pandas.concat([points, new_points])
+	assert isinstance(combined, geopandas.GeoDataFrame)
+	worst_target, worst_dist, pic_for_worst = get_worst_point(
+		combined, targets, use_haversine=use_haversine
+	)
+	print(
+		f'Worst case target after adding new pics: {worst_target}, {format_distance(worst_dist)} from {pic_for_worst}'
+	)
+
+	diffs = find_new_pics_better_individually(
+		points, new_points, targets, use_haversine=use_haversine
+	)
+	diffs = diffs.sort_values('best', ascending=False)
+	for c in ('total', 'best', 'mean'):
+		diffs[c] = diffs[c].map(format_distance)
+	print(diffs)
+
+
 def main() -> None:
 	argparser = ArgumentParser(description=__doc__)
 	argparser.add_argument(
@@ -63,9 +132,16 @@ def main() -> None:
 		'--targets',
 		nargs=ZERO_OR_MORE,
 		type=Path,
-		help='Detect which pics become your best pic from locations or rounds loaded from this path (geojson/csv/ods/etc or submission tracker kml/kmz)',
+		help='Test against locations or rounds loaded from this path (geojson/csv/ods/etc or submission tracker kml/kmz)',
 	)
 	argparser.add_argument('--output-path', type=Path)
+	argparser.add_argument(
+		'--find-if-any-pics-better',
+		action=BooleanOptionalAction,
+		help='Find any instance of a point in new_points being the new closest point for anywhere in targets, has no effect if --targets is not specified. Defaults to true',
+		default=True,
+	)
+	argparser.add_argument('--use-haversine', action=BooleanOptionalAction)
 	# TODO: Option for new_points to just be one point
 	# TODO: lat/lng/blah column name options
 	args = argparser.parse_args()
@@ -78,38 +154,29 @@ def main() -> None:
 
 	distances = get_distances(points, new_points)
 	if args.threshold:
-		distances = distances[distances['distance'] > args.threshold]
+		num_under = (distances['distance'] < args.threshold).sum()
+		if num_under:
+			print(
+				f'Ignoring {num_under} new points as they are within {format_distance(args.threshold)} from existing points'
+			)
+		distances = distances[distances['distance'] >= args.threshold]
 	distances['distance'] = distances['distance'].map(format_distance)
-	distances['point'] = distances['geometry'].map(format_point)
+	distances['coords'] = distances['geometry'].map(format_point)
+	# Remember that coords here is for the new point, not closest, which might be unclear if you come back to look at this code later
 	print('Distances from existing points:')
-	print(distances.drop(columns='geometry'))
+	print(distances.drop(columns='geometry').set_index('new_point'))
+	distances = distances.drop(columns='coords')
 
 	if args.output_path:
 		geodataframe_to_csv(distances, args.output_path, index=False)
 	if args.targets:
-		test_points = load_points_or_rounds(args.targets)
-		test_points = try_set_index_name_col(test_points)
-		if isinstance(test_points.index, RangeIndex):
-			test_points.index = Index(
-				[
-					format_point(geo) if isinstance(geo, Point) else str(geo)
-					for geo in test_points.geometry
-				]
-			)
-		results = find_if_new_pics_better(points, new_points, test_points, use_haversine=True)
-		better = results[results['is_new_better']].copy()
-		better['diff'] = better['current_distance'] - better['new_distance']
-		better = better.sort_values('diff', ascending=False)
-		# TODO: This should be saved to csv, it's just like, do I have a new output_path argument or what
-		for index, row in better.iterrows():
-			diff = row['current_distance'] - row['new_distance']
-			new_pic = row['new_best']
-			old_pic = row['current_best']
-			print(
-				f'{index} would be improved by {new_pic}, beating {old_pic} by {format_distance(diff)}'
-			)
-
-	# TODO: Rather than necessarily the best pic among the new pics, what we probably want to do instead is take each new pic at a time and find how often it becomes the better pic, and sum the distance diffs (the total distance saved by adding this new pic)
+		eval_with_targets(
+			points,
+			new_points,
+			args.targets,
+			find_if_any_pics_better=args.find_if_any_pics_better,
+			use_haversine=args.use_haversine,
+		)
 
 
 if __name__ == '__main__':
