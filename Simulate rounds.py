28f099a9bc2a2e8293ff85a80f2483366c06e1fc
Megan Leet <megan.leet@outlook.com.au>
2025-10-05 00:38:24 +1000

    Simulate rounds: Start doing some stuff that I was going to do

--- Simulate rounds.py
+++ Simulate rounds.py
@@ -6,14 +6,45 @@ from argparse import ArgumentParser, BooleanOptionalAction
 from pathlib import Path
 
 import pandas
-from travelpygame import ScoringOptions, load_rounds, main_tpg_scoring
+from travelpygame import Round, ScoringOptions, load_points, load_rounds, main_tpg_scoring
 from travelpygame.simulation import SimulatedStrategy, simulate_existing_rounds
+from travelpygame.util import format_xy, try_set_index_name_col
+
+
+def compare_rounds(old_round: Round, new_round: Round, name: str | None):
+	if old_round.is_scored:
+		old_winner = max(old_round.submissions, key=lambda sub: sub.score or float('nan'))
+		if old_winner.name != new_round.submissions[0].name:
+			# TODO: Output this somewhere
+			print(
+				f'{old_round.name}: Winner was {old_round.submissions[0].name}, now {new_round.submissions[0].name}'
+			)
+	if name:
+		old_sub = next((sub for sub in old_round.submissions if sub.name == name), None)
+		if not old_sub:
+			return
+		new_sub = next(sub for sub in new_round.submissions if sub.name == name)
+		if old_sub.rank is not None and new_sub.rank is not None and old_sub.rank != new_sub.rank:
+			print(f'{old_round.name}: Placing changed from {old_sub.rank} to {new_sub.rank}')
+		if (old_sub.latitude != new_sub.latitude) or (old_sub.longitude != new_sub.longitude):
+			old_desc = old_sub.description or format_xy(old_sub.longitude, old_sub.latitude)
+			new_desc = new_sub.description or format_xy(new_sub.longitude, new_sub.latitude)
+			print(f'{old_round.name}: Previously submitted {old_desc}, now would submit {new_desc}')
 
 
 def main() -> None:
 	argparser = ArgumentParser(description=__doc__)
 	strategy_choices = {s.name.lower(): s for s in SimulatedStrategy}
 	argparser.add_argument('data_path', type=Path, help='Path to TPG data to use')
+	argparser.add_argument(
+		'--name',
+		help='Optionally keep track of a particular player (likely use case = yourself) and how their submission changes',
+	)
+	argparser.add_argument(
+		'--points-path',
+		type=Path,
+		help="In conjunction with --name, replace your points with those loaded from this file (to use pics that you haven't submitted yet)",
+	)
 	argparser.add_argument(
 		'--output-path', type=Path, help='Output total scores of simulated players here'
 	)
@@ -36,12 +67,13 @@ def main() -> None:
 	)
 	# TODO: Get main TPG data if data_path is not provided (would need to rewrite this as async which isn't necessarily difficult or time consuming but I'm very cbf)
 	# TODO: Many more options - load round targets from point set, generate randomly from regions, etc
-	# TODO: Option to replace/append a particular user's pics with that of a point set (so you can use pics from your voronoi and not just what you've submitted)
 	# TODO: Option to add some point sets as fictional players
 	# TODO: Option to also try with a new_points point set, and see how it compares, and what pics would improve your ranking etc
 	args = argparser.parse_args()
 
 	path: Path = args.data_path
+	name: str | None = args.name
+	points_path: Path | None = args.points_path
 	strategy = strategy_choices[args.strategy]
 	if args.custom_scoring:
 		scoring = ScoringOptions(7500, None, None, None, args.custom_scoring)
@@ -49,18 +81,22 @@ def main() -> None:
 		scoring = main_tpg_scoring
 
 	rounds = load_rounds(path)
-	totals: dict[str, float] = {}
-	for result in simulate_existing_rounds(
+
+	simulation = simulate_existing_rounds(
 		rounds, scoring, strategy, use_haversine=args.use_haversine
-	):
+	)
+	if points_path:
+		if not name:
+			print('Warning: --points-path does not do anything without --name')
+		else:
+			simulation.player_pics[name] = try_set_index_name_col(load_points(points_path)).geometry
+
+	new_rounds = simulation.simulate_rounds()
+	totals: dict[str, float] = {}
+	for result in new_rounds:
 		r = next((r for r in rounds if r.name == result.name), None)
-		if r and r.is_scored:
-			old_winner = max(r.submissions, key=lambda sub: sub.score or float('nan'))
-			if old_winner.name != result.submissions[0].name:
-				# TODO: Output this somewhere
-				print(
-					f'{r.name}: Winner was {r.submissions[0].name}, now {result.submissions[0].name}'
-				)
+		if r:
+			compare_rounds(r, result, name)
 
 		for sub in result.submissions:
 			assert sub.score is not None, 'why is sub.score None'
