17c26662abdd4f08c96c0ac8a294db5fd7f390ab
Megan Leet <megan.leet@outlook.com.au>
2025-10-16 16:32:20 +1100

    Simulate rounds: Fiddle around and refactor so it works with loading submissions from geofiles

--- Simulate rounds.py
+++ Simulate rounds.py
@@ -3,20 +3,14 @@
 
 import logging
 from argparse import ArgumentParser, BooleanOptionalAction
-from collections.abc import Sequence
+from collections.abc import Mapping, Sequence
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 import shapely
+from geopandas.geoseries import GeoSeries
 from pandas import Index, RangeIndex
-from travelpygame import (
-	Round,
-	ScoringOptions,
-	get_submissions_per_user,
-	load_points,
-	load_rounds,
-	main_tpg_scoring,
-)
+from travelpygame import Round, ScoringOptions, load_points, load_rounds, main_tpg_scoring
 from travelpygame.random_points import random_point_in_bbox, random_points_in_poly
 from travelpygame.simulation import (
 	SimulatedStrategy,
@@ -25,6 +19,7 @@ from travelpygame.simulation import (
 	get_round_summary,
 	simulate_existing_rounds,
 )
+from travelpygame.tpg_data import get_submissions_per_user_from_path
 from travelpygame.util import format_point, format_xy, read_geodataframe, try_set_index_name_col
 
 if TYPE_CHECKING:
@@ -32,7 +27,7 @@ if TYPE_CHECKING:
 
 
 def compare_rounds(old_round: Round, new_round: Round, name: str | None):
-	if old_round.is_scored:
+	if old_round.submissions and old_round.is_scored:
 		old_winner = max(old_round.submissions, key=lambda sub: sub.score or float('nan'))
 		if old_winner.name != new_round.submissions[0].name:
 			# TODO: Output this somewhere
@@ -53,8 +48,8 @@ def compare_rounds(old_round: Round, new_round: Round, name: str | None):
 
 
 def get_simulation(
-	existing_rounds: list[Round],
-	pics: dict[str, 'GeoSeries | Sequence[shapely.Point]'],
+	existing_rounds: list[Round] | None,
+	pics: Mapping[str, 'GeoSeries | Sequence[shapely.Point]'],
 	scoring: ScoringOptions,
 	strategy: SimulatedStrategy,
 	targets_path: Path | None,
@@ -62,11 +57,13 @@ def get_simulation(
 	region_path: Path | None,
 	*,
 	use_haversine: bool,
-) -> tuple[Simulation, bool]:
+) -> Simulation:
 	if not targets_path and not num_random_rounds:
+		if existing_rounds is None:
+			raise RuntimeError('You have nothing to simulate')
 		return simulate_existing_rounds(
 			existing_rounds, pics, scoring, strategy, use_haversine=use_haversine
-		), True
+		)
 
 	if targets_path:
 		targets = try_set_index_name_col(load_points(targets_path))
@@ -86,19 +83,17 @@ def get_simulation(
 		rounds = {format_point(point): point for point in points}
 	else:
 		raise RuntimeError('Not sure how we got here')
-	return Simulation(rounds, None, pics, scoring, strategy, use_haversine=use_haversine), False
+	return Simulation(rounds, None, pics, scoring, strategy, use_haversine=use_haversine)
 
 
 def output_results(
 	new_rounds: list[Round],
-	existing_rounds: list[Round],
+	existing_rounds: list[Round] | None,
 	name: str | None,
 	rounds_output_path: Path | None,
 	output_path: Path | None,
-	*,
-	using_existing_rounds: bool = False,
 ):
-	if using_existing_rounds:
+	if existing_rounds:
 		for result in new_rounds:
 			r = next((r for r in existing_rounds if r.name == result.name), None)
 			if r:
@@ -131,16 +126,16 @@ def load_with_name(path: Path | str):
 
 
 def get_pics(
-	rounds: list[Round],
+	pics_per_user: Mapping[str, 'GeoSeries'],
 	name: str | None,
 	points_path: Path | None,
 	threshold: int | None,
 	additional_players: list[list[str]] | None,
-):
+) -> dict[str, 'GeoSeries']:
 	pics = {
-		player: shapely.points([(lng, lat) for lat, lng in latlngs]).tolist()
-		for player, latlngs in get_submissions_per_user(rounds).items()
-		if threshold is None or len(latlngs) >= threshold
+		player: player_pics
+		for player, player_pics in pics_per_user.items()
+		if threshold is None or player_pics.size >= threshold
 	}
 	if additional_players:
 		for additional_name, path in additional_players:
@@ -164,9 +159,9 @@ def main() -> None:
 	argparser = ArgumentParser(description=__doc__)
 	strategy_choices = {s.name.lower(): s for s in SimulatedStrategy}
 	argparser.add_argument(
-		'data_path',
+		'submissions_path',
 		type=Path,
-		help='Path to TPG data to use (to get submissions from), and if a target argument is not specified, the rounds will be used',
+		help='Path to either a geofile with a "player" column to get submissions per player, or to TPG data to get per-user submissions and to use the rounds as targets if targets is not specified',
 	)
 	argparser.add_argument(
 		'--name',
@@ -249,7 +244,7 @@ def main() -> None:
 	# TODO: Option to also try with a new_points point set, and see how it compares, and what pics would improve your ranking etc
 	args = argparser.parse_args()
 
-	path: Path = args.data_path
+	path: Path = args.submissions_path
 	name: str | None = args.name
 	points_path: Path | None = args.points_path
 	targets_path: Path | None = args.targets
@@ -267,10 +262,11 @@ def main() -> None:
 	else:
 		scoring = main_tpg_scoring
 
-	existing_rounds = load_rounds(path)  # Stil need this either way to get the submissions
+	existing_rounds = load_rounds(path) if path.suffix[1:].lower() == 'json' else None
+	pics_per_user = get_submissions_per_user_from_path(path)
 
-	pics = get_pics(existing_rounds, name, points_path, args.threshold, args.add_player)
-	simulation, using_existing_rounds = get_simulation(
+	pics = get_pics(pics_per_user, name, points_path, args.threshold, args.add_player)
+	simulation = get_simulation(
 		existing_rounds,
 		pics,
 		scoring,
@@ -281,21 +277,14 @@ def main() -> None:
 		use_haversine=args.use_haversine,
 	)
 
-	if name not in simulation.player_pics:
+	if name and name not in simulation.player_pics:
 		print(
 			f'Warning: {name} was not found in TPG data or otherwise did not have any pics, so does not exist in this context. Setting to None'
 		)
 		name = None
 
 	new_rounds = simulation.simulate_rounds()
-	output_results(
-		new_rounds,
-		existing_rounds,
-		name,
-		args.rounds_output_path,
-		args.output_path,
-		using_existing_rounds=using_existing_rounds,
-	)
+	output_results(new_rounds, existing_rounds, name, args.rounds_output_path, args.output_path)
 
 
 if __name__ == '__main__':
