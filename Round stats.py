4e3575b7e90cd24a18840f80415b68db8a0f2a7a
Megan Leet <megan.leet@outlook.com.au>
2025-10-26 21:11:00 +1100

    Fiddle around with round stats

--- Round stats.py
+++ Round stats.py
@@ -2,17 +2,21 @@
 """Displays and outputs stats for each round, like the average centroid, etc."""
 
 import asyncio
+import logging
 from argparse import ArgumentParser
 from pathlib import Path
 
 import numpy
 import pandas
+import pyproj
 from aiohttp import ClientSession
 from shapely import MultiPoint, Point
 from travelpygame import load_rounds_async
-from travelpygame.util import get_centroid
+from travelpygame.util import get_centroid, get_projected_crs, get_total_bounds
 
 from lib.format_utils import describe_point
+
+logger = logging.getLogger(Path(__file__).stem)
 
 
 async def main() -> None:
@@ -28,10 +32,31 @@ async def main() -> None:
 		help='Maximum distance in km to count a submission as not being an outlier, default 5000km',
 		default=5000.0,
 	)
+	argparser.add_argument(
+		'--projected-crs',
+		'--metric-crs',
+		help='Name of CRS to use for calculating the centroid, or autodetect',
+	)
 	args = argparser.parse_args()
 
 	rounds = await load_rounds_async(args.path)
-	world_distance: float = args.world_distance
+	world_distance: float = args.world_distance * 1_000
+	total_bounds = get_total_bounds(r.target for r in rounds)
+	print(f'Total bounds: {total_bounds}')
+
+	if args.projected_crs:
+		projected_crs = pyproj.CRS.from_user_input(args.projected_crs)
+	else:
+		projected_crs = get_projected_crs(total_bounds)
+		if projected_crs:
+			logger.info(
+				'Projected CRS autodetected as %s %s', projected_crs.name, projected_crs.srs
+			)
+		else:
+			logger.info(
+				'Could not detect a projected CRS, falling back to World Equidistant Cylindrical'
+			)
+			projected_crs = pyproj.CRS.from_epsg(4087)
 
 	rows = []
 	async with ClientSession() as sesh:
@@ -40,7 +65,6 @@ async def main() -> None:
 				raise ValueError('This has not been scored yet')
 
 			distances = numpy.asarray([sub.distance for sub in r.submissions])
-			distances /= 1_000
 			avg_distance = numpy.mean(distances[distances <= world_distance])
 			avg_distance_raw = numpy.mean(distances)
 
@@ -49,16 +73,16 @@ async def main() -> None:
 			)
 			points = points_raw[distances <= world_distance]
 
-			centroid_raw = get_centroid(MultiPoint(points_raw))
-			centroid = get_centroid(MultiPoint(points))
+			centroid_raw = get_centroid(MultiPoint(points_raw), projected_crs)
+			centroid = get_centroid(MultiPoint(points), projected_crs)
 
 			rows.append(
 				{
 					'Round': r.name or str(r.number),
 					'Number of submissions': len(r.submissions),
 					'Number of submissions within threshold': (distances <= world_distance).sum(),
-					'Average distance': avg_distance,
-					'Raw average distance': avg_distance_raw,
+					'Average distance': avg_distance / 1_000,
+					'Raw average distance': avg_distance_raw / 1_000,
 					'Submission centroid lat': centroid.y,
 					'Submission centroid lng': centroid.x,
 					'Raw centroid lat': centroid_raw.y,
@@ -74,4 +98,5 @@ async def main() -> None:
 
 
 if __name__ == '__main__':
+	logging.basicConfig(level=logging.INFO)
 	asyncio.run(main())
