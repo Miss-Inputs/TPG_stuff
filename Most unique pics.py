ebd1fa00135e058f02643dafea1c3c5e3bc4151e
Megan Leet <megan.leet@outlook.com.au>
2026-09-21 21:53:56 +1000

    Fix most unique pics thingy

--- Most unique pics.py
+++ Most unique pics.py
@@ -21,6 +21,54 @@ from lib.settings import Settings
 
 if TYPE_CHECKING:
 	from pandas import Series
+	from travelpygame.submission_data import SubmissionSummary
+
+
+def get_unique_pics(subs: 'SubmissionSummary', threshold: int) -> geopandas.GeoDataFrame:
+	gdf = geopandas.GeoDataFrame(subs.submissions, geometry='point', crs='wgs84')
+	# TODO: Option to filter for active players only (last submitted something after a certain date)
+
+	rows = []
+	groupie = gdf.groupby('player', sort=False)
+	with tqdm(groupie, total=groupie.ngroups, unit='player') as t:
+		for name, group in t:
+			assert isinstance(group, geopandas.GeoDataFrame), (
+				f'group is {type(group)}, not GeoDataFrame'
+			)
+			t.set_postfix(name=name)
+			n_pics = group.index.size
+			if n_pics < threshold:
+				continue
+
+			others = gdf.drop(index=group.index)
+			other_distances = cartesian_product_distances(group.geometry, others.geometry)
+
+			for index, row in tqdm(group.iterrows(), total=n_pics, leave=False, unit='row'):
+				point = row['point']
+				row_distances: Series = other_distances.loc[index]  # ty:ignore[invalid-argument-type] #.loc should work with Hashable
+				closest_index = row_distances.idxmin()
+				distance = row_distances.loc[closest_index]  # ty: ignore[invalid-argument-type]
+				closest_row: Series = gdf.loc[closest_index]  # ty: ignore[invalid-argument-type]
+				closest = closest_row['point']
+				assert isinstance(closest, Point), f'closest is {type(closest)}, expected Point'
+
+				median = row_distances.median()
+
+				rows.append(
+					{
+						'point': point,
+						'player': name,
+						'mean_dist_to_other': row_distances.mean(),
+						'median_dist_to_other': median,
+						'closest_distance': distance,
+						'closest_lat': closest.y,
+						'closest_lng': closest.x,
+						'closest_user': closest_row['player'],
+					}
+				)
+			del others, other_distances
+	unique_pics = geopandas.GeoDataFrame(rows, geometry='point', crs='wgs84')
+	return unique_pics.sort_values('closest_distance', ascending=False)
 
 
 async def main() -> None:
@@ -43,53 +91,10 @@ async def main() -> None:
 	subs = await load_or_fetch_submission_summary(subs_path)
 	# Do NOT even think about trying to use self_cartesian_product_distances(subs.geometry) to just get vectorized distances all at once. You will accomplish nothing except rendering your computer inoperable for 20 minutes while it runs out of memory and thrashes. Do it whatever the other way is.
 
-	rows = []
-	groupie = subs.groupby('username', sort=False)
-	with tqdm(groupie, total=groupie.ngroups, unit='player') as t:
-		for name, group in t:
-			assert isinstance(group, geopandas.GeoDataFrame), (
-				f'group is {type(group)}, not GeoDataFrame'
-			)
-			t.set_postfix(name=name)
-			n_pics = group.index.size
-			if n_pics < args.threshold:
-				continue
-
-			others = subs.drop(index=group.index)
-			other_distances = cartesian_product_distances(group.geometry, others.geometry)
-
-			for index, row in tqdm(group.iterrows(), total=n_pics, leave=False, unit='row'):
-				point = row.geometry
-				row_distances: Series = other_distances.loc[index]  # ty:ignore[invalid-argument-type] #.loc should work with Hashable
-				closest_index = row_distances.idxmin()
-				distance = row_distances.loc[closest_index]
-				closest_row = subs.loc[closest_index]
-				closest = closest_row.geometry
-				assert isinstance(closest, Point), f'closest is {type(closest)}'
-
-				median = row_distances.median()
-
-				rows.append(
-					{
-						'point': point,
-						'username': name,
-						'player': row['player_name'],
-						'mean_dist_to_other': row_distances.mean(),
-						'median_dist_to_other': median,
-						'closest_distance': distance,
-						'closest_other': closest,
-						'closest_lat': closest.y,
-						'closest_lng': closest.x,
-						'closest_user': closest_row['username'],
-					}
-				)
-			del others, other_distances
-
-	gdf = geopandas.GeoDataFrame(rows, geometry='point', crs='wgs84')
+	gdf = get_unique_pics(subs, args.threshold)
 	gdf = gdf.sort_values('closest_distance', ascending=False)
 	print(gdf)
 	if output_path:
-		# TODO: Clean up the output please and thanks (closest_other should not go into CSV etc, actually it arguably shouldn't go into GeoJSON etc either)
 		# TODO: Also just have something that gets max(closest_distance) per player
 		await asyncio.to_thread(output_geodataframe, gdf, output_path, index=False)
 
